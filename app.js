e19f86b8c180db84070e72e5ad7b4cb52516ee9c
Elvis Mudamba <elvismudamba@gail.com>
2024-09-16 12:16:08 +0300

    all updates

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ let readyyes_btn = document.querySelector(".ready-yes-btn")
 let ready = document.querySelector(".ready")
 let score = 0;
 let scoreTag = document.querySelector(".score")
+let compliments = ["Very nice",
+                   "tremendous",
+                   "beautiful",
+                   "Devine",
+                   "You are smart"
+]
+let randomComplement;
 
 let main = document.querySelector(".main")
 let questionsArray = ["What does HTML stands for?",
-                       //"What does CPU stands for?",
-                       //"//What does GNU stands for",
-                       //"Who created the C programming language?",
-                       //"What does MIDI stands for in music and computers?"
+                       "What does CPU stands for?",
+                       "What does GNU stands for",
+                       "Who created the C programming language?",
+                       "What does MIDI stands for in music and computers?"
 ]
 let answers = ["hypertext markup language",
-               //"central processing unit",
-               //"gnu's not unix",
-               //"dennis richie",
-               //"musical instrument digital interface"
+               "central processing unit",
+               "gnu's not unix",
+               "    ",
+               "musical instrument digital interface"
 ]
 let trials = 3;
 
@@ -24,17 +31,32 @@ readyyes_btn.addEventListener("click", ()=>{
 })
 
 let currentIndex = 0
-
+let flashInterval;
 let gameoverr = false;
 
 function generateQ(){
+    if(gameoverr == true){
+        let game_over = document.createElement("div")
+                        game_over.classList.add("game-over-body")
+                        game_over.innerHTML= `
+                           <div >
+                           <p style ="color: white;  font-size: 1.2rem;">
+                               Game over
+                           </p>
+                         </div>
+                        `
+                        document.querySelector(".question-container").style.display = "none";
+                        document.querySelector(".main").appendChild(game_over)
+        return;
+    }
 
         let createQuiz = document.createElement("div")
         createQuiz.classList.add("question-container")
         createQuiz.innerHTML = `
         <div class="question-container">
          <p id="question" style ="color: white;  font-size: 1.2rem;">
-                    ${questionsArray[currentIndex]}
+              
+                ${currentIndex + 1}. ${questionsArray[currentIndex]}
                 </p>
                 <input id="user-input" type="text">
                 <button id="submit-btn">Submit</button>     
@@ -59,46 +81,37 @@ function generateQ(){
             let userinput = document.querySelector("#user-input").value
             if(userinput != '')
             {
+
+             
                 let change_to_lower = userinput.toLowerCase();
                 //console.log(change_to_lower)
+                randomComplement = Math.floor(Math.random()*compliments.length)
         
                 if(change_to_lower === answers[currentIndex])
                 {    
-                    verdict.innerText = "correct 😎😎😎"
+                    verdict.innerText = `${compliments[randomComplement]} !!! 😎😎😎`
+                    console.log(randomComplement)
                     verdict.style.color = "green"
                     verdict.style.fontSize = "1.8rem"
                     verdict.style.fontWeight = "bold"
                     currentIndex++;
                     score++;
                     scoreTag.innerText = `Score: ${score}`;
+                    flashColor_green()
                     setTimeout(generateQ, 2000)
 
                     if(currentIndex == questionsArray.length){
-                        let game_over = document.createElement("div")
-                        game_over.classList.add("game-over-body")
-                        game_over.innerHTML= `
-                           <div >
-                           <p style ="color: white;  font-size: 1.2rem;">
-                               Game over
-                           </p>
-                         </div>
-                        `
-                        document.querySelector(".question-container").style.display = "none";
-                        document.querySelector(".main").appendChild(game_over)
-                        gameoverr = true
-                        if(gameoverr)
-                        {
-                            
-                        }
                         
+                        gameoverr = true
                     }
-                    
-                    
+   
                 }else{
+                    userinput.value = ' '
                     verdict.innerText = "incorrect 😣😣😣 try again"
                     verdict.style.color = "red"
                     verdict.style.fontSize = "1.8rem"
                     verdict.style.fontWeight = "bold" 
+                    flashColor_red()
                     trials--;
                     remaining_trials.innerText = `${trials} tirals remaining`
                     console.log(trials)
@@ -124,4 +137,24 @@ function generateQ(){
         
             }
     })
+}
+let isGreen = false
+let flashCount = 0;
+const maxFlashes = 10
+
+function flashColor_green()
+{
+        document.body.style.backgroundColor = 'green'
+
+        setTimeout(() =>{
+            document.body.style.backgroundColor = '#2d2d2d';
+        }, 500);
+}
+function flashColor_red(){
+    document.body.style.backgroundColor = 'rgb(255, 44, 44)'
+
+    setTimeout(()=>{
+        document.body.style.backgroundColor = '#2d2d2d'
+    }, 500)
+
 }
